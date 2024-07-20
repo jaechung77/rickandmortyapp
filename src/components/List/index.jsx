7b25fd573fce09fd8811584a9e2e5179b07bfc9b
Jae Chung <jaechung77@gmail.com>
2024-07-21 00:08:30 +1000

    feat: add pagination

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import React, { useState, useEffect } from 'react';
+import { useApolloClient } from '@apollo/client';
 import { CHARACTERS } from '../../graphql/characters';
-import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
 
 function List() {
-  const { data, loading, error } = useQuery(
-    gql`
-      ${CHARACTERS}
-    `
-  );
+  const client = useApolloClient();
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const { data } = await client.query({
+          query: CHARACTERS,
+          variables: { page },
+        });
+        setData(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [client, page]);
 
   if (loading) {
     return <div>...loading</div>;
@@ -18,11 +37,24 @@ function List() {
     return <div>{error.message}</div>;
   }
 
-  return data?.characters?.results?.map((res) => (
-    <div>
-      <p>name: {res.name}</p>
-    </div>
-  ));
+  return (
+    <>
+      {data?.characters?.results?.map((res) => (
+        <div key={res.id}>
+          <p>name: {res.name}</p>
+        </div>
+      ))}
+      {console.log(page)}
+      {page > 1 && (
+        <button onClick={() => setPage((prevPage) => prevPage - 1)}>
+          Prev Page
+        </button>
+      )}
+      <button onClick={() => setPage((prevPage) => prevPage + 1)}>
+        Next Page
+      </button>
+    </>
+  );
 }
 
 export default List;
--- src/graphql/characters.js
+++ src/graphql/characters.js
@@ -1,22 +1,24 @@
-export const CHARACTERS = `
-query Characters {
-    characters(page: 4, filter: null) {
-        results {
-            id
-            name
-            status
-            species
-            type
-            gender
-            image
-            created
-        }
-        info {
-            count
-            pages
-            next
-            prev
-        }
+import { gql } from '@apollo/client';
+
+export const CHARACTERS = gql`
+  query Characters($page: Int!) {
+    characters(page: $page, filter: null) {
+      results {
+        id
+        name
+        status
+        species
+        type
+        gender
+        image
+        created
+      }
+      info {
+        count
+        pages
+        next
+        prev
+      }
     }
-}
+  }
 `;
